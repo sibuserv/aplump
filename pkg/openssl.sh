1e994e6be31a1bd74f1ba6e0df3031d8ac5bd942
Boris Pek <tehnick-8@yandex.ru>
2018-03-02 00:24:08 +0300

    openssl: fix PKG_URL_2

--- pkg/openssl.sh
+++ pkg/openssl.sh
@@ -5,11 +5,12 @@
 (
     PKG=openssl
     PKG_VERSION=1.0.2n
+    PKG_SUBVERSION=1.0.2
     PKG_CHECKSUM=370babb75f278c39e0c50e8c4e7493bc0f18db6867478341a832a982fd15a8fe
     PKG_SUBDIR=${PKG}-${PKG_VERSION}
     PKG_FILE=${PKG}-${PKG_VERSION}.tar.gz
     PKG_URL="ftp://ftp.openssl.org/source/${PKG_FILE}"
-    PKG_URL_2="ftp://ftp.openssl.org/source/old/${OPENSSL_SUBVER}/${PKG_FILE}"
+    PKG_URL_2="ftp://ftp.openssl.org/source/old/${PKG_SUBVERSION}/${PKG_FILE}"
     PKG_DEPS=""
 
     if ! IsPkgInstalled
